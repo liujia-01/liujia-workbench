3f50ce2e90cc17e80414e516e38413545c4855cd
liujia-01 <19565293977@163.com>
2026-07-27 11:28:29 +0800

    Fix service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
 const CACHE='workbench-v1';
-const FILES=['/liujia-workbench/','/liujia-workbench/index.html','/liujia-workbench/manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+self.addEventListener('install',e=>{self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(clients.claim())});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{let r2=res.clone();caches.open(CACHE).then(c=>c.put(e.request,r2));return res})))})
